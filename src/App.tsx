62add384fd7720d2f63854b658eaffbb6e2e7e25
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-06-29 11:30:52 -0300

    feat: :sparkles: Construindo a interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+      <div className="bg-white w-[1100px] mx-auto rounded-xl min-h-[200px] shadow-sm border border-black/20"></div>
+    </div>
+  );
 }
 
 export default App;
