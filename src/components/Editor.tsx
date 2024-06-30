2632fa67ad31eca0ef16e59ee9197a250f1dd7d1
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-06-29 21:25:41 -0300

    feat: :sparkles: Criando o BubbleMenu

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,15 @@
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import CodeBlockLowLight from "@tiptap/extension-code-block-lowlight";
 import { initialContent } from "./initialContent";
 import { common, createLowlight } from "lowlight";
 import js from "highlight.js/lib/languages/javascript";
-
+import {
+  RxFontBold,
+  RxFontItalic,
+  RxStrikethrough,
+  RxCode,
+} from "react-icons/rx";
 import "highlight.js/styles/tokyo-night-dark.css";
 
 const lowlight = createLowlight(common);
@@ -27,9 +32,30 @@ export function Editor() {
     },
   });
   return (
-    <EditorContent
-      editor={editor}
-      className="max-w-[700px] mx-auto pt-16 prose prose-violet"
-    />
+    <>
+      <EditorContent
+        editor={editor}
+        className="max-w-[700px] mx-auto pt-16 prose prose-violet"
+      />
+      {editor && (
+        <BubbleMenu
+          className="bg-white shadow-xl border border-zinc-100 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-100"
+          editor={editor}
+        >
+          <button className="p-2 text-zinc-900 text-sm flex items-center gap-1.5 font-medium leading-none hover:bg-zinc-100">
+            <RxFontBold className="w-4 h-4" />
+          </button>
+          <button className="p-2 text-zinc-900 text-sm flex items-center gap-1.5 font-medium leading-none hover:bg-zinc-100">
+            <RxFontItalic className="w-4 h-4" />
+          </button>
+          <button className="p-2 text-zinc-900 text-sm flex items-center gap-1.5 font-medium leading-none hover:bg-zinc-100">
+            <RxStrikethrough className="w-4 h-4" />
+          </button>
+          <button className="p-2 text-zinc-900 text-sm flex items-center gap-1.5 font-medium leading-none hover:bg-zinc-100">
+            <RxCode className="w-4 h-4" />
+          </button>
+        </BubbleMenu>
+      )}
+    </>
   );
 }
--- src/components/initialContent.ts
+++ src/components/initialContent.ts
@@ -1,17 +1,6 @@
 export const initialContent = `
 <h1>As Vantagens do Tailwind CSS</h1>
-<ul>
-    <li>Controle total sobre o estilo</li>
-    <li>Processo de estilização rápido</li>
-    <li>Responsividade e segurança</li>
-    <li>Recursos adicionais</li>
-</ul>
+<ul><li>Controle total sobre o estilo</li><li>Processo de estilização rápido</li><li>Responsividade e segurança</li><li>Recursos adicionais</li></ul>
 <pre><code class="language-javascript">console.log("Hello, Tailwind CSS!");</code></pre>
-<blockquote>
-"O Tailwind CSS é uma ferramenta poderosa para desenvolvedores
-front-end. Sua abordagem pragmática e modular permite criar interfaces
-incríveis com rapidez."
-</blockquote>
-<p>
-Você pode aprender mais sobre Tailwindcss na <a href="https://rocketseat.com.br">rocketseat.com.br</a>
-</p>`;
+<blockquote>"O Tailwind CSS é uma ferramenta poderosa para desenvolvedores front-end. Sua abordagem pragmática e modular permite criar interfaces incríveis com rapidez."</blockquote>
+<p>Você pode aprender mais sobre Tailwindcss na <a href="https://rocketseat.com.br">rocketseat.com.br</a></p>`;
